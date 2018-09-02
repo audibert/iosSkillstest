be2800cb5e47c0a5e0334468cf7f6eacad6ca82e
Luan Audibert <luan_audibert@hotmail.com>
2018-09-01 22:40:14 -0300

    Criação do projeto, setup das pastas MVC e começo da criação da tela de login

---
+++ CINQSkillsTest/CINQSkillsTest/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CINQSkillsTest
+//
+//  Created by Luan Audibert on 9/1/18.
+//  Copyright © 2018 Luan Audibert. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
